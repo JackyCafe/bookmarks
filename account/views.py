7801e533c4c1194e55a29e53487718171edec065
powerworker1234@gmail.com <powerworker1234@gmail.com>
2020-09-22 17:43:57 +0800

    Initial commit

---
+++ account/views.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
+from account.forms import LoginForm
+from django.contrib.auth.models import User
+
+
+def user_login(request):
+    user : User
+    if request.method =='POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                            username = cd['username'],
+                            password=cd['password'])
+            print()
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    return HttpResponse('Authenticated successfully')
+            else:
+                return HttpResponse(f'Disable account {cd["username"]} {cd["password"]}')
+    else:
+        form = LoginForm()
+        return render(request,'account/login.html',{'form':form})
+
+
+@login_required
+def dashboard(request):
+    return render(request,'account/dashboard.html'
+                  ,{'section':'dashedboard'})
